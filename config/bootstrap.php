72f8ba2813c3a93d78d101371bb8e7d590a55ec4
Yubin Chen <yubin@xseeding.nl>
2016-07-28 16:58:43 +0200

    Zoek functie done

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -143,20 +143,14 @@ if (!Configure::read('App.fullBaseUrl')) {
     unset($httpHost, $s);
 }
 
-$httpHost = env('HTTP_HOST');
-
-if (!empty($httpHost)) {
-    $env = 'production';
-    if (strpos(env('HTTP_HOST'), '.xseeding') !== false) {
-        $env = 'staging';
-    }
-    if (strpos(env('HTTP_HOST'), '.local') !== false || strpos(env('HTTP_HOST'), '.dev.xseeding') !== false) {
-        $env = 'development';
-    }
-    Configure::load('environments/' . $env);
-    
+$env = 'production';
+if (strpos(env('HTTP_HOST'), '.xseeding') !== false) {
+    $env = 'staging';
 }
-
+if (strpos(env('HTTP_HOST'), '.local') !== false || strpos(env('HTTP_HOST'), '.dev.xseeding') !== false) {
+    $env = 'development';
+}
+Configure::load('environments/' . $env);
 
 Cache::config(Configure::consume('Cache'));
 ConnectionManager::config(Configure::consume('Datasources'));
